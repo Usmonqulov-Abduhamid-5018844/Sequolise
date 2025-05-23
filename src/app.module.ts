4e3419664b35aa7d90e320161ee8c8efffec13d4
Usmonqulov-Abduhamid-5018844 <usmonqulovabduhamid00@gmail.com>
2025-05-23 22:47:02 +0500

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ".env"
+  }),
+  SequelizeModule.forRoot({
+    dialect: "postgres",
+    host: process.env.PG_HOST,
+    username: process.env.PG_USER,
+    password: String(process.env.PG_PASS),
+    port: Number(process.env.PG_PORT),
+    database: process.env.PG_DB,
+    synchronize: true,
+    autoLoadModels: true,
+    logging: true,
+    models: []
+  })
+]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
+const Port = Number(process.env.PORT) || 4000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(Port, ()=>{
+    console.log(`Server started on port ${Port}`);
+  });
 }
 bootstrap();
